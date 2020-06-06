8afc2bd33de4cc13cbc9af8be2550713eb073729
Chenjie Wu <54728465+wu3976@users.noreply.github.com>
2020-06-06 03:09:59 -0400

    todo: add server-side things

---
+++ static/tttScript.js
@@ -0,0 +1,261 @@
+status_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+result = 0;
+
+result_count = [0, 0, 0]; // [You win, I win, fair]
+/**
+ * step 1 : check if there is a winning.
+ * @return {number} 0: no win, 1:you win, 2: I win.
+ */
+const judgeWin = () => {
+    if (checkHorizontalCase(1).length === 3
+        || checkVerticalCase(1).length === 3
+        || checkLeftDiagCase(1).length === 3
+        || checkRightDiagCase(1).length === 3){
+        return 1;
+    }
+    if (checkHorizontalCase(2).length === 3
+        || checkVerticalCase(2).length === 3
+        || checkLeftDiagCase(2).length === 3
+        || checkRightDiagCase(2).length === 3){
+        return 2;
+    }
+    if (status_arr.indexOf(0) < 0){ return 3; }
+    return 0;
+}
+/**
+ * step 2: check if opponent is going to win.
+ * @return {[]} the array of positions which is judged or operated.
+ */
+const judgeWouldWinAndSave = () => {
+    let result_arr = checkLeftDiagCase(1);
+    if (result_arr.length === 2){
+        checkLackPosAndInsert(result_arr, 0, status_arr.length, 4);
+        return result_arr;
+    }
+    result_arr = checkRightDiagCase(1);
+    if (result_arr.length === 2){
+        checkLackPosAndInsert(result_arr, 2, 7, 2);
+        return result_arr;
+    }
+    result_arr = checkHorizontalCase(1);
+    console.log("Hori arr: " + result_arr);
+    if (result_arr.length === 2){
+        let start_pos = 3 * Math.floor(result_arr[0] / 3);
+        checkLackPosAndInsert(result_arr, start_pos, start_pos + 3, 1);
+        return result_arr;
+    }
+    result_arr = checkVerticalCase(1);
+    if (result_arr.length === 2){
+        let start_pos = result_arr[0] % 3;
+        checkLackPosAndInsert(result_arr, start_pos, status_arr.length, 3);
+        return result_arr;
+    }
+    return result_arr;
+}
+
+const checkLackPosAndInsert = (result_arr, start, til, step) => {
+    for (let i = start; i < til; i += step){
+        if (result_arr.indexOf(i) < 0){
+            processAction(2, i);
+        }
+    }
+}
+/**
+ * Step 4: See if can place to any diagonal.
+ */
+const judgeDiag = () => {
+    for (let i = 0; i < status_arr.length && i !== 4; i += 2){
+        if (status_arr[i] === 2){
+            let temp_arr = [i + 2, i - 2, i + 3 * 2, i - 3 * 2];
+            // navigate through all neighbours
+            for (let ele of temp_arr){
+                if (ele >= 0 && ele < status_arr.length && status_arr[ele] === 0){
+                    processAction(2, ele);
+                    return true;
+                }
+            }
+        }
+    }
+    // if does not exist diag, or diag's neigh is occupied, prioritize any diag.
+    for (let i = 1; i < status_arr.length; i += 2){
+        if (status_arr[i] === 1){
+            let temp_arr;
+            if (i % 6 === 1){
+                temp_arr = [i + 1, i - 1];
+            } else {
+                temp_arr = [i + 3, i - 3];
+            }
+            let rnd = Math.round(Math.random());
+
+            if (status_arr[temp_arr[rnd]] === 0){
+                processAction(2, temp_arr[rnd]); return true;
+            } else if (status_arr[temp_arr[1 - rnd]] === 0){
+                processAction(2, temp_arr[1 - rnd]); return true
+            }
+        }
+    }
+
+    for (let i = 0; i < status_arr.length && i !== 4; i += 2){
+        if (status_arr[i] === 0){
+            processAction(2, i);
+            return true;
+        }
+    }
+    return false;
+}
+
+
+/**
+ * check horiz case
+ * @param num The number being checked.
+ * @return {[]} One of the largest_length hori occupation array
+ */
+const checkHorizontalCase = (num) => {
+    let result_arr = [];
+    for (let i = 0; i < status_arr.length; i += 3){
+        let temp_arr = [];
+        for (let j = i; j < i + 3; j++){
+            if (status_arr[j] !== 0 && status_arr[j] !== num){
+                temp_arr = [];
+                break;
+            }
+            else if (status_arr[j] === num){
+                temp_arr.push(j);
+            }
+        }
+        result_arr = temp_arr.length > result_arr.length ? temp_arr : result_arr;
+    }
+
+    return result_arr;
+}
+
+/**
+ * vert case
+ * @param num Number checked
+ * @return {[]} One of the largest_length vert occupation array
+ */
+const checkVerticalCase = (num) => {
+    let result_arr = [];
+    for (let i = 0; i < status_arr.length / 3; i++){
+        let temp_arr = [];
+        for (let j = i; j < status_arr.length; j += 3){
+            if (status_arr[j] !== 0 && status_arr[j] !== num){
+                temp_arr = [];
+                break;
+            }
+            else if (status_arr[j] === num){
+                temp_arr.push(j);
+            }
+        }
+        result_arr = temp_arr.length > result_arr.length ? temp_arr : result_arr;
+    }
+    return result_arr;
+}
+
+const checkLeftDiagCase = (num) => {
+    let result_arr = [];
+    for (let i = 0; i < status_arr.length; i += 4){
+        if (status_arr[i] !== 0 && status_arr[i] !== num){
+            result_arr = [];
+            break;
+        }
+         else if (status_arr[i] === num){
+            result_arr.push(i);
+        }
+    }
+    return result_arr;
+}
+
+const checkRightDiagCase = (num) => {
+    let result_arr = [];
+    for (let i = 2; i < 7; i += 2){
+        if (status_arr[i] !== 0 && status_arr[i] !== num){
+            result_arr = [];
+            break;
+        }
+        if (status_arr[i] === num){
+            result_arr.push(i);
+        }
+    }
+    return result_arr;
+}
+
+const next = () => { // check if there is any win at both start and end.
+    let winningResult = judgeWin();
+    if (winningResult !== 0){
+        return winningResult;
+    } else {
+        let arr = judgeWouldWinAndSave();
+        if (arr.length !== 2){ // this means opponent is not going to win
+            if (status_arr[4] === 0){
+                processAction(2, 4);
+            } else if (!judgeDiag()){
+                processAction(2, status_arr.indexOf(0));
+            }
+        }
+    }
+    console.log(status_arr);
+    return judgeWin();
+}
+
+
+// Listener zone
+for (let i = 0; i < status_arr.length; i++){
+    document.getElementById("" + i).addEventListener('click', (event) => {
+        if (result === 0 && status_arr[i] === 0){
+            /*status_arr[i] = 1;
+            document.getElementById("" + i).innerHTML =
+                "<img class='grid-image' src='circle.jpg' alt='circle'>";*/
+            processAction(1, i);
+            result = next();
+            if (result !== 0){
+                let text;
+                if (result === 1){
+                    text = "You win!";
+                } else if (result === 2){
+                    text = "I win!";
+                } else {
+                    text = "Fair!";
+                }
+                result_count[result - 1]++;
+                document.getElementById("status").innerText = text;
+                syncCount();
+            }
+        }
+    });
+}
+
+const syncCount = () => {
+    document.getElementById("count").innerText =
+        "You win: " + result_count[0]
+        + ", I win: " + result_count[1]
+        + ", fair: " + result_count[2];
+}
+
+const reset = () => {
+    status_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < status_arr.length; i++){
+        document.getElementById("" + i).innerHTML = "";
+    }
+    result = 0;
+    document.getElementById("status").innerText = "";
+}
+
+const resetAll = () => {
+    reset();
+    result_count = [0, 0, 0];
+}
+
+const processAction = (player, pos) => {
+    status_arr[pos] = player;
+    if (player === 1){
+        document.getElementById("" + pos).innerHTML =
+            "<img class='grid-image' src='circle.jpg' alt='circle'>";
+    } else {
+        document.getElementById("" + pos).innerHTML =
+            "<img class='grid-image' src='cross.jpg' alt='cross'>";
+    }
+}
+document.getElementById("new_game").addEventListener('click', (event) => {
+    reset();
+})
